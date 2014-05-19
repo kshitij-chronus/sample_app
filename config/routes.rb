685bad5d4c890e6cb517f8ec142fa76d235c0db5
kshitij kumar <kshitij-kumar@chronus.com>
2014-05-19 12:08:01 +0530

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 SampleApp::Application.routes.draw do
-  resources :users
+  resources :users do
+    member do
+      get :following
+      get :followers
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   
   get '/signup',  to: 'users#new'
   get '/signin',  to: 'sessions#new'
